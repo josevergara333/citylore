af95e638baaddbb4ae66b18812d47d90a821e397
josevergara333 <jose.vergara.electronico@gmail.com>
2026-03-21 17:36:45 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,21 @@
+var CACHE_NAME = 'citylore-v1';
+var ASSETS = [
+  '/citylore.html',
+  '/manifest.json'
+];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
